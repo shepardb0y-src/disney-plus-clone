9a71be9d69e5bbbb2036e0304074a5d930a3e854
shepardb0y-src <60862129+shepardb0y-src@users.noreply.github.com>
2022-04-23 18:46:51 -0400

    created nbav bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -70,6 +70,17 @@ a {
     span {
         font-size:13px;
         letter-spacing:1.42px;
+        position:relative;
+       
+        &:after{
+    content:"";
+    height:2px;
+    background:white;
+    position:absolute;
+    left:0;
+    right:0;
+    bottom:-6px;
+    }
     }
 }
 `;
