844c9dc42a5f677cb4fc6d2f49fb465920b334e1
shepardb0y-src <60862129+shepardb0y-src@users.noreply.github.com>
2022-04-23 15:20:51 -0400

    created components and pages folders

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components'
+const Header = () => {
+    return (
+        <Container>
+            Header
+        </Container>
+    );
+}
+
+export default Header;
+
+const Container = styled.div`
+background: black;
+height:70px;
+
+`
