ed44a0be03df3ebe01816a90ef4bc7dc7deac82d
shepardb0y-src <60862129+shepardb0y-src@users.noreply.github.com>
2022-04-23 18:19:40 -0400

    created nbav bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,33 @@ const Header = () => {
   return (
     <Nav>
       <Logo src="./images/logo.svg" />
-      <NavMenu></NavMenu>
+      <NavMenu>
+          <a>
+              <img src="/images/home-icon.svg"/>
+              <span>HOME</span>
+          </a>
+          <a>
+              <img src="/images/search-icon.svg"/>
+              <span>Search</span>
+          </a>
+          <a>
+              <img src="/images/watchlist-icon.svg"/>
+              <span>WATCHLIST</span>
+          </a>
+          <a>
+              <img src="/images/original-icon.svg"/>
+              <span>ORGINAL</span>
+          </a>
+          <a>
+              <img src="/images/movie-icon.svg"/>
+              <span>MOVIE</span>
+          </a>
+          <a>
+              <img src="/images/series-icon.svg"/>
+              <span>SERIES</span>
+          </a>
+      </NavMenu>
+      <UserImage src="/images/2022-03-07 (4).png"></UserImage>
     </Nav>
   );
 };
@@ -21,5 +47,30 @@ padding: 0 36px;
 const Logo = styled.img`
   width: 80px;
 `;
-const NavMenu = styled.div``;
+const UserImage = styled.img`
+  width: 48px;
+  height:48px;
+  border-radius:50%;
+  cursor:pointer;
+  
+`;
+const NavMenu = styled.div`
+display:flex;
+flex:1;
+margin-left:25px;
+align-items:center;
+a {
+    display:flex;
+    align-items:center;
+    padding: 0 12px;
+  cursor:pointer;
+    img{
+        height:20px;
+    }
+    span {
+        font-size:13px;
+        letter-spacing:1.42px;
+    }
+}
+`;
 
