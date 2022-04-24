059fa6d901c8c4adc531b97bd57f645b3dcc27a0
shepardb0y-src <60862129+shepardb0y-src@users.noreply.github.com>
2022-04-24 17:21:36 -0400

    created imageslider

--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -1,11 +1,39 @@
 import React from 'react';
+import styled from 'styled-components'
+import Slider from 'react-slick'
+import "slick-carousel/slick/slick.css"
+import "slick-carousel/slick/slick-theme.css"
+
 
 const ImageSlider = () => {
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay:true
+    }
     return (
-        <div>
-      image
-        </div>
+        <Carousel {...settings}>
+
+      <Wrap>
+          <img src="/images/slider-badging.jpg"/>
+      </Wrap>
+      <Wrap>
+          <img src="/images/slider-badging.jpg"/>
+      </Wrap>
+
+        </Carousel>
     );
 }
 
 export default ImageSlider;
+
+
+const Carousel = styled(Slider)`
+margin-top:20px;
+
+`
+const Wrap = styled.div`
+`
