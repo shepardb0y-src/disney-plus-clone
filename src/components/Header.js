f61fd2edfb5f93f45822f75ffc5bcdea48ffaf1c
shepardb0y-src <60862129+shepardb0y-src@users.noreply.github.com>
2022-04-23 20:10:24 -0400

    added hover effecr to nav bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,30 +5,30 @@ const Header = () => {
     <Nav>
       <Logo src="./images/logo.svg" />
       <NavMenu>
-          <a>
-              <img src="/images/home-icon.svg"/>
-              <span>HOME</span>
-          </a>
-          <a>
-              <img src="/images/search-icon.svg"/>
-              <span>Search</span>
-          </a>
-          <a>
-              <img src="/images/watchlist-icon.svg"/>
-              <span>WATCHLIST</span>
-          </a>
-          <a>
-              <img src="/images/original-icon.svg"/>
-              <span>ORGINAL</span>
-          </a>
-          <a>
-              <img src="/images/movie-icon.svg"/>
-              <span>MOVIE</span>
-          </a>
-          <a>
-              <img src="/images/series-icon.svg"/>
-              <span>SERIES</span>
-          </a>
+        <a>
+          <img src="/images/home-icon.svg" />
+          <span>HOME</span>
+        </a>
+        <a>
+          <img src="/images/search-icon.svg" />
+          <span>Search</span>
+        </a>
+        <a>
+          <img src="/images/watchlist-icon.svg" />
+          <span>WATCHLIST</span>
+        </a>
+        <a>
+          <img src="/images/original-icon.svg" />
+          <span>ORGINAL</span>
+        </a>
+        <a>
+          <img src="/images/movie-icon.svg" />
+          <span>MOVIE</span>
+        </a>
+        <a>
+          <img src="/images/series-icon.svg" />
+          <span>SERIES</span>
+        </a>
       </NavMenu>
       <UserImage src="/images/2022-03-07 (4).png"></UserImage>
     </Nav>
@@ -38,9 +38,9 @@ const Header = () => {
 export default Header;
 
 const Nav = styled.div`
-display:flex;
-align-items:center;
-padding: 0 36px;
+  display: flex;
+  align-items: center;
+  padding: 0 36px;
   background: black;
   height: 70px;
 `;
@@ -49,39 +49,46 @@ const Logo = styled.img`
 `;
 const UserImage = styled.img`
   width: 48px;
-  height:48px;
-  border-radius:50%;
-  cursor:pointer;
-  
+  height: 48px;
+  border-radius: 50%;
+  cursor: pointer;
 `;
 const NavMenu = styled.div`
-display:flex;
-flex:1;
-margin-left:25px;
-align-items:center;
-a {
-    display:flex;
-    align-items:center;
+  display: flex;
+  flex: 1;
+  margin-left: 25px;
+  align-items: center;
+  a {
+    display: flex;
+    align-items: center;
     padding: 0 12px;
-  cursor:pointer;
-    img{
-        height:20px;
+    cursor: pointer;
+    img {
+      height: 20px;
     }
     span {
-        font-size:13px;
-        letter-spacing:1.42px;
-        position:relative;
-       
-        &:after{
-    content:"";
-    height:2px;
-    background:white;
-    position:absolute;
-    left:0;
-    right:0;
-    bottom:-6px;
-    }
-    }
-}
-`;
+      font-size: 13px;
+      letter-spacing: 1.42px;
+      position: relative;
 
+      &:after {
+        content: "";
+        height: 2px;
+        background: white;
+        position: absolute;
+        left: 0;
+        right: 0;
+        bottom: -6px;
+        opacity:0;
+        transform-origin:left center;
+        transform:scaleX(0);
+      }
+    }
+    &:hover {
+     span:after {
+       transform: scaleX(1);
+       opacity: 1;
+     }
+    }
+  }
+`;
