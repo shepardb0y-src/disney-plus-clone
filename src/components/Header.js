0989a90cb80159f0e46e706702ee929da3a67021
shepardb0y-src <60862129+shepardb0y-src@users.noreply.github.com>
2022-04-23 20:20:53 -0400

    added hover effecr to nav bar with bezier transition

--- src/components/Header.js
+++ src/components/Header.js
@@ -80,7 +80,8 @@ const NavMenu = styled.div`
         right: 0;
         bottom: -6px;
         opacity:0;
-        transform-origin:left center;
+        transform-origin: left center;
+        transition:all 250ms cubic-bezier(0.25,0.46,0.45,0.94) 0s;
         transform:scaleX(0);
       }
     }
