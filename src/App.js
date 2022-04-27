df0a015e5c74fb029c8c96b4150407451b874e02
shepardb0y-src <60862129+shepardb0y-src@users.noreply.github.com>
2022-04-27 19:02:23 -0400

    added react router end points

--- src/App.js
+++ src/App.js
@@ -3,19 +3,18 @@ import { Counter } from "./features/counter/Counter";
 import Home from "./pages/Home";
 import "./components/Header";
 import Header from "./components/Header";
-import Detail from "./components/Detail"
-import { BrowserRouter, Route } from "react-router-dom";
+import Detail from "./components/Detail";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
     <div>
       <Header />
-    
-        <Home />
-      
-     
-        <Detail />
-      
+      <Routes>
+        <Route path="/" element={<Home />} />
+
+        <Route path="/detail" element={<Detail />} />
+      </Routes>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import { store } from './app/store';
@@ -7,7 +8,10 @@ import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<React.StrictMode><App/></React.StrictMode>);
+root.render(<React.StrictMode>
+  <BrowserRouter> <App/></BrowserRouter>
+ 
+  </React.StrictMode>);
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
