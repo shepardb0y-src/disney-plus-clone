54e97edf703b04d99b4755a957df7866e8c1f6e8
shepardb0y-src <60862129+shepardb0y-src@users.noreply.github.com>
2022-04-28 10:27:08 -0400

    added background image to detail component

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,11 +1,29 @@
 import React from 'react';
+import styled from 'styled-components'
 
 const Detail = () => {
     return (
-        <div>
-            Detail
-        </div>
+        <Container>
+            <Background>
+                <img src="/images/2019-12-08.png" alt="" />
+            </Background>
+        </Container>
     );
 }
 
 export default Detail;
+
+
+const Container = styled.div`
+min-height:calc(100 -70px);
+pdding: 0 calc(3.5vw + 5px);
+`
+const Background = styled.div`
+position: fixed;
+top:0;
+left:0;
+bottom:0;
+right:0;
+z-index-1;
+opacity:0.8;
+`
