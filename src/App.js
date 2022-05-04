f287387da3a29b0334a5f715fcbc64b15770444f
shepardb0y-src <60862129+shepardb0y-src@users.noreply.github.com>
2022-05-04 09:08:36 -0400

    created login page conpmonent and route

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from "./pages/Home";
 import "./components/Header";
 import Header from "./components/Header";
 import Detail from "./components/Detail";
+import Login from "./components/Login";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const App = () => {
@@ -13,6 +14,7 @@ const App = () => {
       <Routes>   
         <Route path="/" element={<Home />} />
         <Route path="/detail" element={<Detail />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </div>
   );
